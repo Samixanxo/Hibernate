5f405328aec298a0a4ef5b4baea97f2e66a9c686
Samuel Robayo <samirobayo04@gmail.com>
2024-03-31 19:51:55 +0200

    #5

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -15,138 +15,147 @@ import model.Entrada;
 import model.Vid;
 import utils.TipoVid;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manager {
-	private static Manager manager;
-	private ArrayList<Entrada> entradas;
-	private Session session;
-	private Transaction tx;
-	private Bodega b;
-	private Campo c;
+    private static Manager manager;
+    private List<Entrada> entradas;
+    private List<Campo> campos;
+    private Session session;
+    private Transaction tx;
+    private Bodega b;
 
-	private Manager () {
-		this.entradas = new ArrayList<>();
-	}
-	
-	public static Manager getInstance() {
-		if (manager == null) {
-			manager = new Manager();
-		}
-		return manager;
-	}
-	
-	private void createSession() {
-		org.hibernate.SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    	session = sessionFactory.openSession();
-	}
+    private Manager() {
+        this.entradas = new ArrayList<>();
+        this.campos = new ArrayList<>();
+    }
 
-	public void init() {
-		createSession();
-		getEntrada();
-		manageActions();
-		showAllCampos();
-		showMaxBodega();
-		session.close();
-	}
+    public static Manager getInstance() {
+        if (manager == null) {
+            manager = new Manager();
+        }
+        return manager;
+    }
 
-	private void manageActions() {
-		for (Entrada entrada : this.entradas) {
-			try {
-				System.out.println(entrada.getInstruccion());
-				switch (entrada.getInstruccion().toUpperCase().split(" ")[0]) {
-					case "B":
-						addBodega(entrada.getInstruccion().split(" "));
-						break;
-					case "C":
-						addCampo(entrada.getInstruccion().split(" "));
-						break;
-					case "V":
-						addVid(entrada.getInstruccion().split(" "));
-						break;
-					case "#":
-						vendimia();
-						break;
-					default:
-						System.out.println("Instruccion incorrecta");
-				}
-			} catch (HibernateException e) {
-				e.printStackTrace();
-				if (tx != null) {
-					tx.rollback();
-				}
-			}
-		}
-	}
+    public void init() {
+        createSession();
+        getEntradas();
+        manageActions();
+        showAllCampos();
+        showMaxBodega();
+        session.close();
+    }
 
-	private void vendimia() {
-		this.b.getVids().addAll(this.c.getVids());
-		c.recolected();
-		
-		tx = session.beginTransaction();
-		session.save(b);
-		
-		tx.commit();
-	}
+    private void createSession() {
+        org.hibernate.SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        session = sessionFactory.openSession();
+    }
 
-	private void addVid(String[] split) {
-		Vid v = new Vid(TipoVid.valueOf(split[1].toUpperCase()), Integer.parseInt(split[2]));
-		tx = session.beginTransaction();
-		session.save(v);
-		
-		c.addVid(v);
-		session.save(c);
-		
-		tx.commit();
-		
-	}
+    private void manageActions() {
+        for (Entrada entrada : entradas) {
+            try {
+                System.out.println(entrada.getInstruccion());
+                switch (entrada.getInstruccion().toUpperCase().split(" ")[0]) {
+                    case "B":
+                        addBodega(entrada.getInstruccion().split(" "));
+                        break;
+                    case "C":
+                        addCampo(entrada.getInstruccion().split(" "));
+                        break;
+                    case "V":
+                        addVid(entrada.getInstruccion().split(" "));
+                        break;
+                    case "#":
+                        vendimia();
+                        break;
+                    default:
+                        System.out.println("Instrucción incorrecta");
+                }
+            } catch (HibernateException e) {
+                e.printStackTrace();
+                if (tx != null) {
+                    tx.rollback();
+                }
+            }
+        }
+    }
 
-	private void addCampo(String[] split) {
-		c = new Campo(b);
-		tx = session.beginTransaction();
-		
-		int id = (Integer) session.save(c);
-		c = session.get(Campo.class, id);
-		
-		tx.commit();
-	}
+    private void vendimia() {
+        for (Campo campo : campos) {
+            this.b.getVids().addAll(campo.getVids());
+            campo.recolected();
+            tx = session.beginTransaction();
+            session.save(campo);
+            tx.commit();
+        }
 
-	private void addBodega(String[] split) {
-		b = new Bodega(split[1]);
-		tx = session.beginTransaction();
-		
-		int id = (Integer) session.save(b);
-		b = session.get(Bodega.class, id);
-		
-		tx.commit();
-		
-	}
+        tx = session.beginTransaction();
+        session.save(b);
+        tx.commit();
+    }
 
-	private void getEntrada() {
-		tx = session.beginTransaction();
-		Query q = session.createQuery("select e from Entrada e");
-		this.entradas.addAll(q.list());
-		tx.commit();
-	}
+    private void addVid(String[] split) {
+        if (campos.isEmpty()) {
+            System.out.println("No hay campos registrados para agregar vid.");
+            return;
+        }
 
-	private void showAllCampos() {
-		tx = session.beginTransaction();
-		Query q = session.createQuery("select c from Campo c");
-		List<Campo> list = q.list();
-		for (Campo c : list) {
-			System.out.println(c);
-		}
-		tx.commit();
-	}
-	
-	private void showMaxBodega() {
-	    tx = session.beginTransaction();
-	    Query query = session.createQuery("FROM Bodega ORDER BY id DESC");
-	    query.setMaxResults(1); 
-	    Bodega maxBodega = (Bodega) query.uniqueResult(); 
-	    System.out.println("Bodega con el ID más alto: " + maxBodega);
-	    tx.commit();
-	}
-	
-	
+        Vid v = new Vid(TipoVid.valueOf(split[1].toUpperCase()), Integer.parseInt(split[2]));
+        Campo c = campos.get(campos.size() - 1); // Get the latest added Campo
+        c.addVid(v);
 
-	
+        tx = session.beginTransaction();
+        session.save(v);
+        session.save(c);
+        tx.commit();
+    }
+
+    private void addCampo(String[] split) {
+        if (b == null) {
+            System.out.println("No hay bodega registrada para agregar campo.");
+            return;
+        }
+
+        Campo c = new Campo(b);
+        campos.add(c); // Add the Campo to the list
+
+        tx = session.beginTransaction();
+        session.save(c);
+        tx.commit();
+    }
+
+    private void addBodega(String[] split) {
+        b = new Bodega(split[1]);
+
+        tx = session.beginTransaction();
+        session.save(b);
+        tx.commit();
+    }
+
+    private void getEntradas() {
+        tx = session.beginTransaction();
+        Query q = session.createQuery("select e from Entrada e");
+        entradas.addAll(q.list());
+        tx.commit();
+    }
+
+    private void showAllCampos() {
+        tx = session.beginTransaction();
+        Query q = session.createQuery("select c from Campo c");
+        campos.addAll(q.list()); // Populate the campos list
+        for (Campo c : campos) {
+            System.out.println(c);
+        }
+        tx.commit();
+    }
+
+    private void showMaxBodega() {
+        tx = session.beginTransaction();
+        Query query = session.createQuery("FROM Bodega ORDER BY id DESC");
+        query.setMaxResults(1);
+        Bodega maxBodega = (Bodega) query.uniqueResult();
+        System.out.println("Bodega con el ID más alto: " + maxBodega);
+        tx.commit();
+    }
 }
--- src/model/Campo.java
+++ src/model/Campo.java
@@ -17,47 +17,46 @@ import javax.persistence.Table;
 @Entity
 @Table(name= "campo")
 public class Campo {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "id", unique = true, nullable = true)
-	private int id_campo;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = true)
+    private int id_campo;
 
-	@OneToMany
+    @OneToMany
     @JoinColumn(name = "campo_id")
-	private List<Vid> vids;
-	
-	@OneToOne
-	@JoinColumn(name = "id_bodega")
-	private Bodega bodega;
-	
-	@Column(name = "recolectado", columnDefinition = "boolean default false")
-	private boolean recolectado;
-	
-	public Campo() {}
+    private List<Vid> vids;
 
-	public Campo(Bodega b) {
-		this.bodega = b;
-		this.vids = new ArrayList<>();
-	}
+    @OneToOne
+    @JoinColumn(name = "id_bodega")
+    private Bodega bodega;
 
-	@Override
-	public String toString() {
-		return "Campo [id_campo=" + id_campo + ", vids=" + Arrays.toString(vids.toArray()) + ", bodega="
-				+ bodega.toString() + "]";
-	}
+    @Column(name = "recolectado", columnDefinition = "boolean default false")
+    private boolean recolectado;
 
-	public void addVid(Vid v) {
-		this.vids.add(v);		
-	}
+    public Campo() {}
 
-	public ArrayList<Vid> getVids() {
-		ArrayList<Vid> vids = new ArrayList<>();
-		vids.addAll(this.vids);
-		return vids;
-	}
-	
-	public void recolected()
-	{
-		  this.recolectado = true;
-	}
+    public Campo(Bodega bodega) {
+        this.bodega = bodega;
+        this.vids = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        String bodegaString = bodega != null ? bodega.toString() : "No Bodega associated"; // Handle null bodega
+        return "Campo [id_campo=" + id_campo + ", vids=" + Arrays.toString(vids.toArray()) + ", bodega=" + bodegaString + "]";
+    }
+
+    public void addVid(Vid v) {
+        this.vids.add(v);
+    }
+
+    public ArrayList<Vid> getVids() {
+        ArrayList<Vid> vids = new ArrayList<>();
+        vids.addAll(this.vids);
+        return vids;
+    }
+
+    public void recolected() {
+        this.recolectado = true;
+    }
 }
